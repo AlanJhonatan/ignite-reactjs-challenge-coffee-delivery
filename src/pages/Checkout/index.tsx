12b883611a65ae92e32665da7106aa6e7f80385b
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-21 17:27:18 -0300

    feat: implemented basic checkout form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,80 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollarSimple,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import {
+  CheckoutContainer,
+  OrderForm,
+  OrderFormHeader,
+  OrderFormInput,
+  PaymentForm,
+  PaymentFormHeader,
+  PaymentMethodButton,
+  PaymentMethodWrapper,
+  SubmitOrderContainer,
+} from './styles'
+
 export function Checkout() {
-  return <h1>Checkout Screen</h1>
+  return (
+    <CheckoutContainer>
+      <div>
+        <h3>Complete seu pedido</h3>
+
+        <OrderForm>
+          <OrderFormHeader>
+            <MapPinLine size={22} />
+            <div>
+              <span>Endereço de entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </OrderFormHeader>
+
+          <OrderFormInput type="text" placeholder="CEP" />
+          <OrderFormInput type="text" placeholder="Rua" />
+          <OrderFormInput type="text" placeholder="Número" />
+          <OrderFormInput type="text" placeholder="Complemento" />
+          <OrderFormInput type="text" placeholder="Bairro" />
+          <OrderFormInput type="text" placeholder="Cidade" />
+          <OrderFormInput type="text" placeholder="UF" />
+        </OrderForm>
+
+        <PaymentForm>
+          <PaymentFormHeader>
+            <CurrencyDollarSimple size={22} />
+            <div>
+              <span>Pagamento</span>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </PaymentFormHeader>
+
+          <PaymentMethodWrapper>
+            <PaymentMethodButton>
+              <CreditCard size={18} />
+              <p>Cartão de Crédito</p>
+            </PaymentMethodButton>
+
+            <PaymentMethodButton>
+              <Bank size={18} />
+              <p>Débito</p>
+            </PaymentMethodButton>
+
+            <PaymentMethodButton>
+              <Money size={18} />
+              <p>Dinheiro</p>
+            </PaymentMethodButton>
+          </PaymentMethodWrapper>
+        </PaymentForm>
+      </div>
+
+      <div>
+        <h3>Cafés selecionados</h3>
+        <SubmitOrderContainer />
+      </div>
+    </CheckoutContainer>
+  )
 }
