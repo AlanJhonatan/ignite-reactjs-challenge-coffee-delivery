92e55f3e88717d1d0af81dff6f40592f9b76df82
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-23 20:21:25 -0300

    feat: fixed layout bottom margin

--- src/Layout/style.ts
+++ src/Layout/style.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 74rem;
   margin: 0 auto;
-
-  /* padding: 0 10rem; */
+  margin-bottom: 1.5rem;
 
   display: flex;
   flex-direction: column;
